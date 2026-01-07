fe89a2c3cf7eeefdf7324f0d1772cc31e3b849e4
Diana Forostiana <dianaforost@gmail.com>
2026-01-07 21:11:50 +0100

    starter changes

--- header.php
+++ header.php
@@ -5,41 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">    
     <?php wp_head(); ?>
-    <title>It-volunteers</title>
+    <title>All Canadian Emblem Corporation</title>
 </head>
 <body>  
-    <div class="wrapper">
-        <header class="header">
-            <div class="header__content _container">
-                <div class="header__menu menu">
-                    <div class="menu__icon icon-menu menu__round">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                </div>
-                <div class="menu__nav">  
-                    <?php 
-                        if ( has_custom_logo() ) {
-                            echo get_custom_logo();
-                        }
-                    ?>                     
-                    <div class="menu__content">IT VOLUNTEERS</div>
-                    <nav class="menu__body"> 
-                        <div class=" menu__container">
-                            <?php wp_nav_menu( [
-                                'theme_location'       => 'header',                          
-                                'container'            => false,                           
-                                'menu_class'           => 'menu__list',
-                                'menu_id'              => false,    
-                                'echo'                 => true,                            
-                                'items_wrap'           => '<ul id="%1$s" class="header_list %2$s">%3$s</ul>',  
-                                ] ); 
-                            ?>   
-                        </div>                          
-                    </nav> 
-                    <div class="burger-menu__overlay"></div> 
-                </div>                
-            </div>                      
-        </header>  
+     <header class="header">
+        <div class="container">
+            <div class="header__content">
+               <div><a href="http://all-can-emb/">All Canadian Emblem Corporation</a></div>
+               <div>
+                <ul class="header__page-list">
+                <li><a href="">Home</a></li>
+                <li><a href="">What We Do</a></li>
+                <li><a href="">About us</a></li>
+                <li><a href="">Contact</a></li>
+               </ul>
+            </div>
+            </div>
+        </div>
+    </header>
 	
