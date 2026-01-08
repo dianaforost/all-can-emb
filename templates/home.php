a99d2aa8ccf6be8e007fef714b18a4e4cf6d16c7
Diana Forostiana <dianaforost@gmail.com>
2026-01-08 19:17:52 +0100

    main section custom fields

--- templates/home.php
+++ templates/home.php
@@ -6,15 +6,30 @@ get_header();
 ?>
     <main>
 <section class="section" style ="height: 600px;">
-    <div class ="section-image">
+    <div class ="section-image" style="background-image: url('<?php  echo get_field('main_picture'); ?>')">
     </div>
       <div class ="container">
        <div class ="content">
          <ul>
-            <li> <h1 class="main-title">All Canadian Emblem Corporation</h1></li>
-            <li> <p class="main-text">Leading North American manufacturer of high-quality chenille patches, 
-            embroidered crests, and custom garment decorations for teams, schools, and brands.</p></li>
-            <li> <button class="main-button">View our work</button></li>
+            <li> <?php  if (get_field( 'main_title' )) {?>
+                    <h1 class="main-title">
+                        <?php the_field("main_title")?>
+                    </h1>
+                    <?php } ?></li>
+            <li><?php  if (get_field( 'main_text' )) {?>
+                    <p class="main-text">
+                        <?php the_field("main_text")?>
+            </p>
+                    <?php } ?>
+              </li>
+            <li>
+
+            <?php  if (get_field( 'main_button_text' )) {?>
+                    <button class="main-button">
+                        <?php the_field("main_button_text")?>
+            </button>
+                    <?php } ?>
+              </li>
          </ul>
        </div>
 </div>
