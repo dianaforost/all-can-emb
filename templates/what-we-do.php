1fd765afe733ac0c509ff1c47ffbe1752dfea52a
Diana Forostiana <dianaforost@gmail.com>
2026-01-12 19:07:00 +0100

    "All" category

--- templates/what-we-do.php
+++ templates/what-we-do.php
@@ -29,6 +29,26 @@ $categories = get_categories([
 ]); ?>
 <?php if ( $categories ) : ?>
     <ul class="what-we-do__category__list">
+       
+ <?php
+            $all_cat_id = get_cat_ID('All');
+            $all_category_image = get_field('category_image', 'category_' . $all_cat_id);
+            ?>
+            <li class="what-we-do__category__list__item">
+                <a class="what-we-do__category__list__item__link__wrap" href="<?php echo esc_url( get_category_link( $all_cat_id ) ); ?>">
+                    <?php if ( $all_category_image && isset($all_category_image['url']) ) : ?>
+                        <img class="what-we-do__category__list__item__image"
+                             src="<?php echo esc_url( $all_category_image['url'] ); ?>"
+                             alt="All">
+                        <div class="what-we-do__category__list__item__image__wrap"></div>
+                    <?php endif; ?>
+                    <div class="what-we-do__category__list__item__link">
+                        <p><?php echo esc_html('All'); ?></p>
+                    </div>
+                </a>
+            </li>
+
+
         <?php foreach ( $categories as $category ) : ?>
             <?php
         get_template_part(
