1039130b49c3db2b194e20b4f6fd0d71cce5b632
Diana Forostiana <dianaforost@gmail.com>
2026-01-12 18:37:52 +0100

    Changed styles for categories

--- template-parts/one-category.php
+++ template-parts/one-category.php
@@ -17,14 +17,14 @@ $category_image = get_field( 'category_image', 'category_' . $category->term_id 
 <li class="what-we-do__category__list__item">
   <a class="what-we-do__category__list__item__link__wrap" href="<?php echo esc_url( $category_link ); ?>">
       <?php if ( $category_image['url'] ) : ?>
+        <img class="what-we-do__category__list__item__image" 
+        src="<?php echo esc_url( $category_image['url'] ); ?>" 
+        alt="<?php echo esc_attr( $category->name ); ?>">
         <div class="what-we-do__category__list__item__image__wrap">
-          <img class="what-we-do__category__list__item__image" 
-          src="<?php echo esc_url( $category_image['url'] ); ?>" 
-          alt="<?php echo esc_attr( $category->name ); ?>">
         </div>
         <?php endif; ?>
             <div class="what-we-do__category__list__item__link">
-              <?php echo esc_html( $category->name ); ?>
+              <p><?php echo esc_html( $category->name ); ?></p>
             </div>
     </a>
 </li>
