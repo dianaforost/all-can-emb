e93d6014df3087bf2f4b8495a725404f2833f350
Diana Forostiana <dianaforost@gmail.com>
2026-01-13 22:59:16 +0100

    contacts title section

--- templates/contacts.php
+++ templates/contacts.php
@@ -7,7 +7,14 @@ get_header();
 <main>
   <section class="section">
     <div class="container">
-      <div class="inner-container"></div>
+      <div class="inner-container">
+        <?php $contacts_title=get_field('contacts_title');
+        $contacts_text= get_field('contacts_text'); 
+        if($contacts_title & $contacts_text):?>
+        <h2><?php echo esc_html($contacts_title) ?></h2>
+        <p> <?php echo esc_html($contacts_text) ?></p>
+        <?php endif ?>
+      </div>
     </div>
   </section>
 </main>
