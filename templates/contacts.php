78e2dbe0ed5f5ee2190cd9527661d92ded015464
Diana Forostiana <dianaforost@gmail.com>
2026-01-14 18:12:38 +0100

    our team styles

--- templates/contacts.php
+++ templates/contacts.php
@@ -26,14 +26,14 @@ get_header();
           <?php while ( have_rows( 'our_team_member' ) ) : the_row();?>
           <li class="our-team__member__item">
            <div class="our-team__member__item__wrap">
-             <h4><?php echo get_sub_field('our_team_member_full_name') ?></h4>
-              <p><?php echo get_sub_field('our_team_member_title') ?></p> 
+             <h4 class="our-team__member__item__name"><?php echo get_sub_field('our_team_member_full_name') ?></h4>
+              <p class="our-team__member__item__title"><?php echo get_sub_field('our_team_member_title') ?></p> 
            </div>
             <?php if(have_rows('our_team_member_contacts')) :?>
             <div class="our-team__member__item__contacts">
               <?php while (have_rows('our_team_member_contacts')) : the_row(); ?>
               <div class="our-team__member__item__contacts__wrap">
-              <p><?php echo get_sub_field('our_team_member_contact_title') ?></p>
+              <p class="our-team__member__item__contacts__wrap__title"><?php echo get_sub_field('our_team_member_contact_title') ?>:</p>
 
 
               <?php if ( have_rows('our_team_member_contact_repeater') ) : ?>
