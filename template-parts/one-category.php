8282c80fdab457d1a44f27770951443fc71d238f
Diana Forostiana <dianaforost@gmail.com>
2026-01-16 17:35:19 +0100

    What we do categories
    Custom Post Types & Taxonomies

--- template-parts/one-category.php
+++ template-parts/one-category.php
@@ -10,21 +10,28 @@ if ( ! $category ) {
     return;
 }
 
-$category_link  = get_category_link( $category->term_id );
-$category_image = get_field( 'category_image', 'category_' . $category->term_id );
+$category_image = get_field(
+    'category_image',
+    'samples-categories_' . $category->term_id
+);
+
+$category_link = get_term_link( $category );
 ?>
 
 <li class="what-we-do__category__list__item">
-  <a class="what-we-do__category__list__item__link__wrap" href="<?php echo esc_url( $category_link ); ?>">
-      <?php if ( $category_image['url'] ) : ?>
-        <img class="what-we-do__category__list__item__image" 
-        src="<?php echo esc_url( $category_image['url'] ); ?>" 
-        alt="<?php echo esc_attr( $category->name ); ?>">
-        <div class="what-we-do__category__list__item__image__wrap">
-        </div>
-        <?php endif; ?>
-            <div class="what-we-do__category__list__item__link">
-              <p><?php echo esc_html( $category->name ); ?></p>
-            </div>
-    </a>
+  <a class="what-we-do__category__list__item__link__wrap"
+     href="<?php echo esc_url($category_link); ?>">
+
+    <?php if ( $category_image && isset($category_image['url']) ) : ?>
+      <img class="what-we-do__category__list__item__image"
+           src="<?php echo esc_url($category_image['url']); ?>"
+           alt="<?php echo esc_attr($category->name); ?>">
+      <div class="what-we-do__category__list__item__image__wrap"></div>
+    <?php endif; ?>
+
+    <div class="what-we-do__category__list__item__link">
+      <p><?php echo esc_html($category->name); ?></p>
+    </div>
+
+  </a>
 </li>
