7282ff3d6f37ee661f4ab5faba0ba23b97fd4cc4
Diana Forostiana <dianaforost@gmail.com>
2026-01-19 23:17:59 +0100

    Breadcrumbs All category fix

--- taxonomy-samples-categories.php
+++ taxonomy-samples-categories.php
@@ -5,42 +5,72 @@ get_header();
 
 <main>
   <?php get_template_part( 'template-parts/breadcrumbs' ); ?>
+
 <section class="section category">
   <div class="container">
     <div class="inner-container">
       <h2 class="category__title"><?php echo esc_html( single_term_title('', false) ); ?></h2>
-      <?php if (have_posts()) : ?>
-      <ul class="category__list">
-        <?php while( have_posts()) : the_post(); ?>
-        <?php
-        $sample_title = get_field('sample_title');
-        $sample_image = get_field('sample_image');
-        $sample_link = get_permalink(); 
-        $sample_category = '';
-        $categories = get_the_terms( get_the_ID(), 'samples-categories' );
-        if ( $categories && ! is_wp_error( $categories ) ) {
-          foreach ( $categories as $category ) {
-            if ( $category->slug === 'all' ) continue;
-            $sample_category = $category->name;
-            break;
+
+      <?php
+      $current_term = get_queried_object();
+
+      if ( $current_term && $current_term->slug === 'all' ) {
+          // Custom query: get all samples
+          $args = [
+              'post_type' => 'samples',
+              'posts_per_page' => -1,
+              'orderby'        => 'date'
+          ];
+          $samples = new WP_Query($args);
+      } else {
+          $samples = new WP_Query([
+              'post_type' => 'samples',
+              'tax_query' => [
+                  [
+                      'taxonomy' => 'samples-categories',
+                      'field'    => 'slug',
+                      'terms'    => $current_term->slug,
+                  ],
+              ],
+              'posts_per_page' => -1
+          ]);
+      }
+
+      if ( $samples->have_posts() ) : ?>
+        <ul class="category__list">
+          <?php while( $samples->have_posts() ) : $samples->the_post(); ?>
+          <?php
+          $sample_title = get_field('sample_title');
+          $sample_image = get_field('sample_image');
+          $sample_link = get_permalink(); 
+
+          // Get sample category (skip "all")
+          $sample_category = '';
+          $categories = get_the_terms( get_the_ID(), 'samples-categories' );
+          if ( $categories && ! is_wp_error( $categories ) ) {
+              foreach ( $categories as $category ) {
+                  if ( $category->slug === 'all' ) continue;
+                  $sample_category = $category->name;
+                  break;
+              }
           }
-        }
-        ?>
+          ?>
           <?php get_template_part(
-            'template-parts/one-sample',
-            null,
-            [
-              'sample_title' => $sample_title,
-              'sample_image' => $sample_image,
-              'sample_link' => $sample_link,
-              'sample_category' => $sample_category
-            ]
+              'template-parts/one-sample',
+              null,
+              [
+                  'sample_title' => $sample_title,
+                  'sample_image' => $sample_image,
+                  'sample_link' => $sample_link,
+                  'sample_category' => $sample_category
+              ]
           ); ?>
-        <?php endwhile; ?>
-      </ul>
+          <?php endwhile; ?>
+        </ul>
       <?php else : ?>
         <p>No samples found</p>
       <?php endif; ?>
+      <?php wp_reset_postdata(); ?>
     </div>
   </div>
 </section>
