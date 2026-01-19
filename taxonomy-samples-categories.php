794ce3ad97c9f0f27e2742a7d70b277dcc49017f
Diana Forostiana <dianaforost@gmail.com>
2026-01-19 18:39:10 +0100

    Samples category

--- taxonomy-samples-categories.php
+++ taxonomy-samples-categories.php
@@ -7,20 +7,30 @@ get_header();
 <section class="section category">
   <div class="container">
     <div class="inner-container">
-      <h2><?php echo esc_html( single_term_title('', false) ); ?></h2>
+      <h2 class="category__title"><?php echo esc_html( single_term_title('', false) ); ?></h2>
       <?php if (have_posts()) : ?>
-      <ul>
+      <ul class="category__list">
         <?php while( have_posts()) : the_post(); ?>
         <?php
         $sample_title = get_field('sample_title');
         $sample_image = get_field('sample_image');
+        $sample_category = '';
+        $categories = get_the_terms( get_the_ID(), 'samples-categories' );
+        if ( $categories && ! is_wp_error( $categories ) ) {
+          foreach ( $categories as $category ) {
+            if ( $category->slug === 'all' ) continue;
+            $sample_category = $category->name;
+            break;
+          }
+        }
         ?>
           <?php get_template_part(
             'template-parts/one-sample',
             null,
             [
               'sample_title' => $sample_title,
-              'sample_image' => $sample_image
+              'sample_image' => $sample_image,
+              'sample_category' => $sample_category
             ]
           ); ?>
         <?php endwhile; ?>
--- template-parts/one-sample.php
+++ template-parts/one-sample.php
@@ -1,19 +1,26 @@
 <?php
 $sample_title = $args['sample_title'] ?? '';
 $sample_image = $args['sample_image'] ?? null;
+$sample_category=$args['sample_category'] ?? '';
 ?>
 
-<li class="one-sample">
+<li class="sample">
 
   <?php if ( $sample_image ) : ?>
-    <img
+    <img class="sample__image"
       src="<?php echo esc_url( $sample_image['url'] ); ?>"
       alt="<?php echo esc_attr( $sample_image['alt'] ); ?>"
     >
   <?php endif; ?>
+  
+<div class="sample__wrap">
+  <?php if( $sample_category) : ?>
+  <p class="sample__category"> <?php echo esc_html($sample_category) ?></p>
+<?php endif; ?>
 
   <?php if ( $sample_title ) : ?>
-    <h3><?php echo esc_html( $sample_title ); ?></h3>
+    <h3 class="sample__title"><?php echo esc_html( $sample_title ); ?></h3>
   <?php endif; ?>
+</div>
 
 </li>
