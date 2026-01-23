0d0b976e57f1641ee88c191a6b489e552a0b1009
Diana Forostiana <dianaforost@gmail.com>
2026-01-23 14:43:39 -0600

    you might also like section

--- single-samples.php
+++ single-samples.php
@@ -42,8 +42,71 @@ get_header();
         if ($quote_button_text && $quote_button_link):?>
         <a class="button" href="<?php echo esc_url($quote_button_link['url']); ?>"><?php echo esc_html($quote_button_text); ?></a>
         <?php endif ?>
-        
      </div>
+    </div>
+  </div>
+</section>
+
+<section class="section">
+  <div class="container">
+    <div class="inner-container">
+      <?php $you_might_like_title= get_field('you_might_like_title', 'option'); 
+      if($you_might_like_title):?>
+      <h3 class="you-might-like__title"><?php echo esc_html($you_might_like_title); ?></h3>
+      <?php endif ?>
+
+     <?php
+$terms = get_the_terms( get_the_ID(), 'samples-categories' );
+
+if ( $terms && ! is_wp_error( $terms ) ) :
+    $term_ids = [];
+    foreach ( $terms as $term ) {
+            $term_ids[] = $term->term_id;
+    }
+
+    if ( ! empty( $term_ids ) ) :
+        $args = [
+            'post_type'      => 'samples',
+            'posts_per_page' => 3,
+            'post__not_in'   => [ get_the_ID() ],
+            'tax_query'      => [
+                [
+                    'taxonomy' => 'samples-categories',
+                    'field'    => 'term_id',
+                    'terms'    => $term_ids,
+                ],
+            ],
+        ];
+
+        $related_samples = new WP_Query( $args );
+
+        if ( $related_samples->have_posts() ) :
+        ?>
+            <ul class="you-might-like__list">
+                <?php while ( $related_samples->have_posts() ) : $related_samples->the_post(); ?>
+
+                    <?php
+                    get_template_part(
+                        'template-parts/one-sample',
+                        null,
+                        [
+                            'sample_title' => get_the_title(),
+                            'sample_image' => get_field('sample_image'),
+                            'sample_link'  => get_permalink(),
+                        ]
+                    );
+                    ?>
+
+                <?php endwhile; ?>
+            </ul>
+        <?php
+        endif;
+
+        wp_reset_postdata();
+
+    endif;
+endif;
+?>
     </div>
   </div>
 </section>
