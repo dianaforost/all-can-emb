953a7c61e88e30f118133fa5f2af63c7270fafeb
Diana Forostiana <dianaforost@gmail.com>
2026-01-23 11:38:47 -0600

    pagination color fix

--- single-samples.php
+++ single-samples.php
@@ -5,9 +5,25 @@ get_header();
 
 <main id="primary" class="site-main">
   <?php get_template_part( 'template-parts/breadcrumbs' ); ?>
+<?php
+// Get all categories (terms) for this sample
+$categories = get_the_terms( get_the_ID(), 'samples-categories' );
+
+if ( $categories && ! is_wp_error( $categories ) ) :
+    $cat_links = [];
+
+    foreach ( $categories as $category ) {
+        $cat_links[] = '<a href="' . esc_url( get_term_link( $category ) ) . '">' . esc_html( $category->name ) . '</a>';
+    }
+
+    // Join multiple categories with comma
+    echo implode( ', ', $cat_links );
+    echo '</p>';
+endif;
+?>
 <h1><?php the_title(); ?></h1>
 <?php $image=get_field('sample_image'); ?>
-<?php echo $image['url'] ?>
+<img src="<?php echo $image['url'] ?>" alt="<?php echo $image['alt']?>"  />
 
 </main>
 
